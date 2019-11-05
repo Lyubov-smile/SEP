d87ad024640863d3567ad7dfedf193f0d6d0553c
Lyubov-smile <voronyak.lyubov@gmail.com>
2019-11-05 22:23:57 +0200

    Ht2_ex1

---
+++ Statements_syntax/task01.py
@@ -0,0 +1,10 @@
+# 1. Write a Python program to check if a value exists in an array.
+
+s = ["Red", "Green", "Blue", "White"]
+
+print('Original array:')
+print(s)
+print("Check if 'Green' in color array!")
+print('Green' in s)
+print("Check if 'Pink' in color array!")
+print('Pink' in s)
