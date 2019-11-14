4adcb8c359179f505e15e9160e06057fde6093a2
Lyubov-smile <voronyak.lyubov@gmail.com>
2019-11-14 20:01:35 +0200

    add task37

--- Statements_syntax/task37.py
+++ Statements_syntax/task37.py
@@ -1,23 +1,29 @@
-#
+# 37. In a two-dimensional array of order M and N, swap the specified columns.
 
-n = int(input('Input size of array: '))
-m = int(input('Input size of array: '))
+row = int(input('Enter the number of rows: '))
+col = int(input('Enter the number of columns: '))
 
 arr = []
-for i in range(n):
+for i in range(row):
     arr.append([])
-    for j in range(m):
-        r = int(input("Insert element: "))
-        arr[i].append(r)
+    for j in range(col):
+        el = int(input("Insert element: "))
+        arr[i].append(el)
 
+n = int(input('Input the first value of row between o and '+str(col)+': '))
+m = int(input('Input the second value of row between o and '+str(col)+': '))
 
+print('The two-dimensional array of order '+str(row)+' and '+str(col)+' is: ')
+for i in arr:
+    print(i)
 
-print(arr)
+arr_new = arr.copy()
 
-arr2 = [[] for i in range(n)]
-for i in range(n):
-    for j in range(m):
-        r = int(input("Insert element: "))
-        arr2[i].append(r)
+for i in range(row):
+    temp = arr_new[i][n]
+    arr_new[i][n] = arr_new[i][m]
+    arr_new[i][m] = temp
 
-print(arr2)
+print('The array with changed columns is: ')
+for i in arr_new:
+    print(i)
